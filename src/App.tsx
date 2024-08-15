67892e68326804f636565705af07d7d9380bd04d
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-15 12:26:20 +0300

    Appended routes to post main page, single post and edit single post

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { Navbar } from './components/Navbar'
-import { PostList } from './features/posts/PostList'
-import { AddPostForm } from './features/posts/AddPostForm'
+import { PostsMainPage } from './features/posts/PostsMainPage'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { EditPostForm } from './features/posts/EditPostForm'
 
 function App() {
   return (
@@ -10,15 +11,9 @@ function App() {
       <Navbar />
       <div className="App">
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <AddPostForm />
-                <PostList />
-              </>
-            }
-          ></Route>
+          <Route path="/" element={<PostsMainPage />}></Route>
+          <Route path="/posts/:postId" element={<SinglePostPage />}></Route>
+          <Route path="/editPost/:postId" element={<EditPostForm />}></Route>
         </Routes>
       </div>
     </Router>
