37550f3b7c935184117e6321a02183601fa48a01
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-15 14:47:46 +0300

    Appended user reducer and combined it to root reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import type { Action } from '@reduxjs/toolkit'
 import postReducer from '@/features/posts/postsSlice'
+import usersReducer from '@/features/users/usersSlice'
 
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
   reducer: {
     // Declare that `state.counter` will be updated by the `counterReducer` function
     posts: postReducer,
+    users: usersReducer,
   },
 })
 
