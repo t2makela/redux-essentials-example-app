478d1e806b9e43693cfb98ffb4ad4f1cfcacf5a4
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-15 12:18:37 +0300

    Appended post main page component

---
+++ src/features/posts/PostsMainPage.tsx
@@ -0,0 +1,11 @@
+import { AddPostForm } from './AddPostForm'
+import { PostList } from './PostList'
+
+export const PostsMainPage = () => {
+  return (
+    <>
+      <AddPostForm />
+      <PostList />
+    </>
+  )
+}
