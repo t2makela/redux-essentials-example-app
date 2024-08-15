d85008f668f54a3dfaee854906cfda1441797162
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-15 12:20:30 +0300

    Appended link to single post page from post list

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -1,12 +1,16 @@
 import { useAppSelector } from '@/app/hooks'
+import { Link } from 'react-router-dom'
+import { selectAllPosts } from './postsSlice'
 
 export const PostList = () => {
   // Select the `state.posts` value from the store into the component
-  const posts = useAppSelector((state) => state.posts)
+  const posts = useAppSelector(selectAllPosts)
 
   const renderedPosts = posts.map((post) => (
     <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
+      <h3>
+        <Link to={`posts/${post.id}`}>{post.title}</Link>
+      </h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
     </article>
   ))
