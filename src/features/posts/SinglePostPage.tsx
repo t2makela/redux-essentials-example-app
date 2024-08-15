4f565b520381e8fd61ca3fc91509d600c9559a52
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-15 12:19:08 +0300

    Appended single post page component

---
+++ src/features/posts/SinglePostPage.tsx
@@ -0,0 +1,30 @@
+import { Link, useParams } from 'react-router-dom'
+
+import { useAppSelector } from '@/app/hooks'
+import { selectPostById } from './postsSlice'
+
+export const SinglePostPage = () => {
+  const { postId } = useParams()
+
+  const post = useAppSelector((state) => selectPostById(state, postId!))
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    )
+  }
+
+  return (
+    <section>
+      <article className="post">
+        <h2>{post.title}</h2>
+        <p className="post-content">{post.content}</p>
+        <Link to={`/editPost/${post.id}`} className="button">
+          Edit Post
+        </Link>
+      </article>
+    </section>
+  )
+}
