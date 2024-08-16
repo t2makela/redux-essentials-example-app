9fbe401d68a61b0cf3d3c407aff31eaaa1a67322
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-16 10:15:39 +0300

    Modified post form so that logged in user is author post added post

--- src/features/posts/AddPostForm.tsx
+++ src/features/posts/AddPostForm.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { nanoid } from '@reduxjs/toolkit'
 import { useAppDispatch, useAppSelector } from '@/app/hooks'
 import { type Post, postAdded } from './postsSlice'
-import { selectAllUsers } from '../users/usersSlice'
+import { selectCurrentUsername } from '../auth/authSlice'
 
 // TS types for the input fields
 // See: https://epicreact.dev/how-to-type-a-react-form-on-submit-handler/
@@ -18,7 +18,7 @@ interface AddPostFormElements extends HTMLFormElement {
 export const AddPostForm = () => {
   // Get the 'dispatch' method from store
   const dispatch = useAppDispatch()
-  const users = useAppSelector(selectAllUsers)
+  const userId = useAppSelector(selectCurrentUsername)!
   const handleSubmit = (e: React.FormEvent<AddPostFormElements>) => {
     // Prevent server submission
     e.preventDefault()
@@ -26,7 +26,6 @@ export const AddPostForm = () => {
     const { elements } = e.currentTarget
     const title = elements.postTitle.value
     const content = elements.postContent.value
-    const userId = elements.postAuthor.value
 
     dispatch(postAdded(title, content, userId))
 
@@ -34,11 +33,6 @@ export const AddPostForm = () => {
 
     e.currentTarget.reset()
   }
-  const usersOptions = users.map((user) => (
-    <option key={user.id} value={user.id}>
-      {user.name}
-    </option>
-  ))
 
   return (
     <section>
@@ -47,10 +41,6 @@ export const AddPostForm = () => {
         <label htmlFor="postTitle">Post Title:</label>
         <input type="text" id="postTitle" defaultValue="" required />
         <label htmlFor="postAuthor">Author:</label>
-        <select id="postAuthor" name="postAuthor" required>
-          <option value=""></option>
-          {usersOptions}
-        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea id="postContent" name="postContent" defaultValue="" required />
         <button>Save Post</button>
