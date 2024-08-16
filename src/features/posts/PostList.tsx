c4f819dfc26349653b467db76a01c5ac6807b9af
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-16 10:13:05 +0300

    Appended reaction buttons and fixed link path

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { selectAllPosts } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostList = () => {
   // Select the `state.posts` value from the store into the component
@@ -11,11 +12,12 @@ export const PostList = () => {
   const renderedPosts = orderedPosts.map((post) => (
     <article className="post-excerpt" key={post.id}>
       <h3>
-        <Link to={`posts/${post.id}`}>{post.title}</Link>
+        <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </h3>
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <PostAuthor userId={post.user} />
       <TimeAgo timestamp={post.date} />
+      <ReactionButtons post={post} />
     </article>
   ))
 
