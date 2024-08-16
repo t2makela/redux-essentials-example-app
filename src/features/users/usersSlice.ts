4f3759f47dc72da7976bba5e0e9dc63717627de3
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-16 10:03:36 +0300

    Appended current user selector to user reducer

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-
 import type { RootState } from '@/app/store'
+import { selectCurrentUsername } from '../auth/authSlice'
 
 interface User {
   id: string
@@ -25,3 +25,8 @@ export const selectAllUsers = (state: RootState) => state.users
 
 export const selectUserById = (state: RootState, userId: string | null) =>
   state.users.find((user) => user.id === userId)
+
+export const selectCurrentUser = (state: RootState) => {
+  const currentUsername = selectCurrentUsername(state)
+  return selectUserById(state, currentUsername)
+}
