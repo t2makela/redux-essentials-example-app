2f1731a7aa3b4f0cd788c6b7501a20ab622994a1
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-23 10:33:15 +0300

    Appended notifications reducer to store configuration

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,6 +2,7 @@ import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit'
 import postReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
 import authReducer from '@/features/auth/authSlice'
+import notificationsReducer from '@/features/notifications/notificationSlice'
 
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
@@ -9,6 +10,7 @@ export const store = configureStore({
     auth: authReducer,
     posts: postReducer,
     users: usersReducer,
+    notifications: notificationsReducer,
   },
 })
 
