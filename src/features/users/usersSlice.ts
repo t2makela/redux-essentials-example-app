a7b1a530629b5416aeaecb8b9c024162704f05d3
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-23 08:35:26 +0300

    Appended async action to fetch users from API. Initial user state is set to empty

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,22 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import type { RootState } from '@/app/store'
 import { selectCurrentUsername } from '../auth/authSlice'
+import { client } from '@/api/client'
+import { createAppAsyncThunk } from '@/app/withTypes'
 
 interface User {
   id: string
   name: string
 }
 
+export const fetchUsers = createAppAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get<User[]>('fakeApi/users')
+  return response.data
+})
+
 const initialState: User[] = [
-  { id: '0', name: 'Tianna Jenkins' },
+  /* { id: '0', name: 'Tianna Jenkins' },
   { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
+  { id: '2', name: 'Madison Price' }, */
 ]
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
