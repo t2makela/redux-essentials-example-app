dd34377d59c4bbcc2506c5b137b7ba9150596f11
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-26 15:51:42 +0300

    Appended listener middleware and appended listener to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,6 +3,7 @@ import postReducer from '@/features/posts/postsSlice'
 import usersReducer from '@/features/users/usersSlice'
 import authReducer from '@/features/auth/authSlice'
 import notificationsReducer from '@/features/notifications/notificationsSlice'
+import { listenerMiddleware } from './listenerMiddleware'
 
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
@@ -12,6 +13,7 @@ export const store = configureStore({
     users: usersReducer,
     notifications: notificationsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(listenerMiddleware.middleware),
 })
 
 // Infer the type of `store`
