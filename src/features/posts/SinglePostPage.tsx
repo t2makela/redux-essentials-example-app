cac202bd2c896fbab2bafbe9b1e58b76d7c258cd
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-28 13:22:58 +0300

    usePostQuery into use in single post page

--- src/features/posts/SinglePostPage.tsx
+++ src/features/posts/SinglePostPage.tsx
@@ -1,18 +1,16 @@
 import { Link, useParams } from 'react-router-dom'
-
 import { useAppSelector } from '@/app/hooks'
-import { selectPostById } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from '@/components/TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
 import { selectCurrentUsername } from '@/features/auth/authSlice'
+import { useGetPostQuery } from '../api/apiSlice'
+import { Spinner } from '@/components/Spinner'
 
 export const SinglePostPage = () => {
   const { postId } = useParams()
-
-  const post = useAppSelector((state) => selectPostById(state, postId!))
   const currentUsername = useAppSelector(selectCurrentUsername)!
-
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId!)
   if (!post) {
     return (
       <section>
@@ -21,21 +19,28 @@ export const SinglePostPage = () => {
     )
   }
   const canEdit = currentUsername === post.user
+  let content: React.ReactNode
 
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <p className="post-content">{post.content}</p>
-        <PostAuthor userId={post.user} />
-        <TimeAgo timestamp={post.date} />
-        <ReactionButtons post={post} />
-        {canEdit && (
-          <Link to={`/editPost/${post.id}`} className="button">
-            Edit Post
-          </Link>
-        )}
-      </article>
-    </section>
-  )
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
+      <section>
+        <article className="post">
+          <h2>{post.title}</h2>
+          <p className="post-content">{post.content}</p>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+          <ReactionButtons post={post} />
+          {canEdit && (
+            <Link to={`/editPost/${post.id}`} className="button">
+              Edit Post
+            </Link>
+          )}
+        </article>
+      </section>
+    )
+  }
+
+  return <section>{content}</section>
 }
