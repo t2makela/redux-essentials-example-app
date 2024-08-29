ebcfabb7ac8847fed3e1e9dd8893218cbff0fb9a
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2024-08-29 11:44:56 +0300

    User list from the RTK query cache entry and selectors source also from there

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,12 +1,13 @@
-import { createSlice, createEntityAdapter } from '@reduxjs/toolkit'
+import { createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit'
 import type { RootState } from '@/app/store'
 import { selectCurrentUsername } from '../auth/authSlice'
 import { client } from '@/api/client'
 import { createAppAsyncThunk } from '@/app/withTypes'
+import { apiSlice } from '@/features/api/apiSlice'
 
 const usersAdapter = createEntityAdapter<User>()
 
-interface User {
+export interface User {
   id: string
   name: string
 }
@@ -29,9 +30,22 @@ const usersSlice = createSlice({
 
 export default usersSlice.reducer
 
+const emptyUsers: User[] = []
+
+export const selectUsersResult = apiSlice.endpoints.getUsers.select()
+
+export const selectAllUsers = createSelector(selectUsersResult, (usersResult) => usersResult?.data ?? emptyUsers)
+
+export const selectUserById = createSelector(
+  selectAllUsers,
+  (state: RootState, userId: string) => userId,
+  (users, userId) => users.find((user) => user.id === userId),
+)
+
+/* Temporarily ignore adapter selectors - we'll come back to this later
 export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(
   (state: RootState) => state.users,
-)
+) */
 
 export const selectCurrentUser = (state: RootState) => {
   const currentUsername = selectCurrentUsername(state)
